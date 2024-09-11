233e580ad602eb49e8b00d0688355bc686f7493f
carfdev <jose.carlos@fdez.se>
2024-09-11 20:15:05 +0200

    extend collors and fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "beauty-bush": {
+          50: "#fcf4f4",
+          100: "#fae9ea",
+          200: "#f5d6da",
+          300: "#efc1c7",
+          400: "#e18b99",
+          500: "#d16276",
+          600: "#bb435e",
+          700: "#9d334e",
+          800: "#842d46",
+          900: "#722940",
+          950: "#3e131f",
+        },
+      },
+      fontFamily: {
+        alegreya: ["Alegreya", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
