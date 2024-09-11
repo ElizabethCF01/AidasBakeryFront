769e46c40bd6f6202ebd932a234a8425c03076fe
carfdev <jose.carlos@fdez.se>
2024-09-11 20:14:48 +0200

    add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://delicate-mooncake-705514.netlify.app",
   integrations: [tailwind(), sitemap()]
 });
